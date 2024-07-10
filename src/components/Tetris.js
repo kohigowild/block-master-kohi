e407fc323c402884554eacc39c75c17c02fc917e
kohi <mainmirror14@gmail.com>
2024-07-10 21:49:00 +0900

    update : Stage update and player movement

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { createStage } from '../gameHelpers'
+
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris'
 
 import { usePlayer } from '../hooks/usePlayer'
@@ -13,11 +15,40 @@ export default function Tetris() {
   const [dropTime, setDropTime] = useState(null)
   const [gameOver, setGameOver] = useState(false)
 
-  const [player] = usePlayer()
+  const [player, updatePlayerPos, resetPlayer] = usePlayer()
   const [stage, setStage] = useStage(player)
 
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 })
+  }
+
+  const startGame = () => {
+    setStage(createStage())
+    resetPlayer()
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop()
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1)
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
+  }
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -30,7 +61,7 @@ export default function Tetris() {
               <Display text='Level' />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
