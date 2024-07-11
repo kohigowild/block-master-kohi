ec1c3d6856c4fae97f501e982527957497362e34
kohi <mainmirror14@gmail.com>
2024-07-11 20:58:02 +0900

    update : Clear Rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from '../gameHelpers'
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage())
+  const [rowsCleared, setRowsCleared] = useState(0)
 
   useEffect(() => {
+    setRowsCleared(0)
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((ack, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1)
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']))
+          return ack
+        }
+        ack.push(row)
+        return ack
+      }, [])
+
     const updateStage = (prevStage) => {
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -13,16 +27,21 @@ export const useStage = (player, resetPlayer) => {
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
-            newStage[y + player.pos.y][x + player.pos.x] = [
-              value,
-              `${player.collided ? 'merged' : 'clear'}`,
-            ]
+            const newY = y + player.pos.y
+            const newX = x + player.pos.x
+            if (newStage[newY] && newStage[newY][newX]) {
+              newStage[newY][newX] = [
+                value,
+                `${player.collided ? 'merged' : 'clear'}`,
+              ]
+            }
           }
         })
       })
 
       if (player.collided) {
         resetPlayer()
+        return sweepRows(newStage)
       }
 
       return newStage
