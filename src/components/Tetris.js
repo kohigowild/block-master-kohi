046c8d3c2b7228f074bcca0ff299b022561fa4b0
kohi <mainmirror14@gmail.com>
2024-07-11 20:29:26 +0900

    update : Collision Detection

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { createStage } from '../gameHelpers'
+import { checkCollision, createStage } from '../gameHelpers'
 
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris'
 
@@ -16,19 +16,30 @@ export default function Tetris() {
   const [gameOver, setGameOver] = useState(false)
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer()
-  const [stage, setStage] = useStage(player)
+  const [stage, setStage] = useStage(player, resetPlayer)
 
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 })
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 })
+    }
   }
 
   const startGame = () => {
     setStage(createStage())
     resetPlayer()
+    setGameOver(false)
   }
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false })
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false })
+    } else {
+      if (player.pos.y < 1) {
+        setGameOver(true)
+        setDropTime(null)
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true })
+    }
   }
 
   const dropPlayer = () => {
